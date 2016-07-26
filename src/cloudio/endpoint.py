9619f3602d52b9225bed56fd0747bd444bcd66ba
boozo-unlimited <thomas.sterren@gmail.com>
2016-07-26 10:01:51 +0200

    Updated MqttAsyncClient Updated CloudioEndpoint certificates handling

--- src/cloudio/endpoint.py
+++ src/cloudio/endpoint.py
@@ -9,6 +9,7 @@ from cloudio.properties_endpoint_configuration import PropertiesEndpointConfigur
 from utils.resource_loader import ResourceLoader
 from cloudio.cloudio_node import CloudioNode
 from message_format.json_format import JsonMessageFormat
+from utils import path_helpers
 
 class CloudioEndpoint():
     """Internal Endpoint structure used by CloudioEndpoint.
@@ -17,7 +18,13 @@ class CloudioEndpoint():
     """
 
     # Constants ######################################################################################
-    MQTT_HOST_URI_PROPERTY = 'ch.hevs.cloudio.endpoint.hostUri'
+    MQTT_HOST_URI_PROPERTY = u'ch.hevs.cloudio.endpoint.hostUri'
+
+    CERT_AUTHORITY_FILE_PROPERTY = u'ch.hevs.cloudio.endpoint.ssl.authorityCert'
+
+    ENDPOINT_IDENTITY_FILE_PROPERTY = u'ch.hevs.cloudio.endpoint.ssl.clientCert'        # PKCS12 based file (*.p12)
+    ENDPOINT_IDENTITY_CERT_FILE_PROPERTY = u'ch.hevs.cloudio.endpoint.ssl.clientCert'   # (*.pem)
+    ENDPOINT_IDENTITY_KEY_FILE_PROPERTY = u'ch.hevs.cloudio.endpoint.ssl.clientKey'     # (*.pem)
 
     def __init__(self, uuid, configuration=None):
 
@@ -43,9 +50,16 @@ class CloudioEndpoint():
 
         self.options = MqttConnectOptions()
 
-        # TODO: Remove _username and _password
+        self.options._caFile = configuration.getProperty(self.CERT_AUTHORITY_FILE_PROPERTY, None)
+        self.options._clientCertFile = configuration.getProperty(self.ENDPOINT_IDENTITY_CERT_FILE_PROPERTY, None)
+        self.options._clientKeyFile = configuration.getProperty(self.ENDPOINT_IDENTITY_KEY_FILE_PROPERTY, None)
         self.options._username = configuration.getProperty('username')
         self.options._password = configuration.getProperty('password')
+
+        # Make path usable
+        self.options._caFile = path_helpers.prettify(self.options._caFile)
+        self.options._clientCertFile = path_helpers.prettify(self.options._clientCertFile)
+        self.options._clientKeyFile = path_helpers.prettify(self.options._clientKeyFile)
 
         self.mqtt = MqttAsyncClient(host, clientId=self.uuid, clean_session=self.cleanSession)
 
@@ -82,20 +96,20 @@ class CloudioEndpoint():
                 if self.isOnline():
                     data = self.messageFormat.serializeNode(node)
                     print data
-                    self.mqtt.publish('@nodeAdded/' + node.getUuid().toString(), data, 1, False)
+                    self.mqtt.publish(u'@nodeAdded/' + node.getUuid().toString(), data, 1, False)
 
 
     def run(self):
         """Called by the internal thread"""
 
-        print 'Cloudio endpoint thread running...'
+        print u'Cloudio endpoint thread running...'
 
         while not self.mqtt.isConnected():
             try:
-                self.mqtt.connect(self.options._username, self.options._password)
+                self.mqtt.connect(self.options)
             except:
-                print 'Error during broker connect!'
-                exit(0)
+                print u'Error during broker connect!'
+                exit(1)
 
             if not self.mqtt.isConnected():
                 # If we should not retry, give up
@@ -121,6 +135,6 @@ class CloudioEndpoint():
 
     def announce(self):
         # Send birth message
-        print 'Sending birth message...'
-        strMessage = 'TBD'
-        self.mqtt.publish('@online/' + self.uuid, strMessage, 1, True)
+        print u'Sending birth message...'
+        strMessage = u'TBD'
+        self.mqtt.publish(u'@online/' + self.uuid, strMessage, 1, True)
--- src/cloudio/mqtt_helpers.py
+++ src/cloudio/mqtt_helpers.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import time
+import os, time
 import paho.mqtt.client as mqtt
 import ssl
 
@@ -14,12 +14,34 @@ class MqttAsyncClient():
 
         self.client.on_connect = self.onConnect
 
-    def connect(self, username, password):
-        self.client.username_pw_set(username, password=password)
+    def connect(self, options):
+
+        if options._caFile:
+            # Check if file exists
+            if not os.path.isfile(options._caFile):
+                raise RuntimeError(u'CA file \'%s\' does not exist!' % options._caFile)
+
+        clientCertFile = None
+        if options._clientCertFile:
+            # Check if file exists
+            if not os.path.isfile(options._clientCertFile):
+                raise RuntimeError(u'Client certificate file \'%s\' does not exist!' % options._clientCertFile)
+            else:
+                clientCertFile = options._clientCertFile
+
+        clientKeyFile = None
+        if options._clientKeyFile:
+            # Check if file exists
+            if not os.path.isfile(options._clientKeyFile):
+                raise RuntimeError(u'Client private key file \'%s\' does not exist!' % options._clientKeyFile)
+            else:
+                clientKeyFile = options._clientKeyFile
+
+        self.client.username_pw_set(options._username, password=options._password)
         self.client.tls_insecure_set(True)  # True: No verification of the server hostname in the server certificate
-        self.client.tls_set('/Users/sth/.config/cloud.io/ca.pem',    # CA certificate
-               #             '/Users/sth/.config/cloud.io/devel-8725b7b47d85-cert.pem',  # Client private key
-               #             '/Users/sth/.config/cloud.io/devel-8725b7b47d85-key.pem',   # Client certificate
+        self.client.tls_set(options._caFile,    # CA certificate
+                            certfile=clientCertFile,  # Client certificate
+                            keyfile=clientKeyFile,    # Client private key
                             tls_version=ssl.PROTOCOL_TLSv1,                     # ssl.PROTOCOL_TLSv1, ssl.PROTOCOL_TLSv1_2
                             ciphers=None)      # None, 'ALL', 'TLSv1.2', 'TLSv1.0'
 
@@ -33,20 +55,20 @@ class MqttAsyncClient():
     def onConnect(self, client, userdata, flags, rc):
         if rc == 0:
             self._isConnected = True
-            print 'Info: Connection to cloudio broker established.'
+            print u'Info: Connection to cloudio broker established.'
         else:
             if rc == 1:
-                print 'Error: Connection refused - incorrect protocol version'
+                print u'Error: Connection refused - incorrect protocol version'
             elif rc == 2:
-                print 'Error: Connection refused - invalid client identifier'
+                print u'Error: Connection refused - invalid client identifier'
             elif rc == 3:
-                print 'Error: Connection refused - server unavailable'
+                print u'Error: Connection refused - server unavailable'
             elif rc == 4:
-                print 'Error: Connection refused - bad username or password'
+                print u'Error: Connection refused - bad username or password'
             elif rc == 5:
-                print 'Error: Connection refused - not authorised'
+                print u'Error: Connection refused - not authorised'
             else:
-                print 'Error: Connection refused - unknown reason'
+                print u'Error: Connection refused - unknown reason'
 
             if rc != 3: # Expect for 'server unavailable'
                 # Close application
@@ -59,3 +81,6 @@ class MqttConnectOptions():
     def __init__(self):
         self._username = ''
         self._password = ''
+        self._caFile = None                 # type: str
+        self._clientCertFile = None         # type: str
+        self._clientKeyFile = None          # type: str
