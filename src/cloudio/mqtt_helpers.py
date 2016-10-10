e5296139a0bb756ef5d7eac87cf2ac9eaeea9955
boozo-unlimited <thomas.sterren@gmail.com>
2016-10-10 09:29:01 +0200

    Updated MqttClient Persistence class Now class is a real interface Updated methods to provide same interface as in the paho java version

--- src/cloudio/mqtt_helpers.py
+++ src/cloudio/mqtt_helpers.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os, time
+from abc import ABCMeta, abstractmethod
 import paho.mqtt.client as mqtt     # pip install paho-mqtt
 import ssl
 
@@ -121,10 +122,85 @@ class MqttConnectOptions():
         self.will['qos'] = qos
         self.will['retained'] = retained
 
-class MqttClientPersistence():
-    """Mimic the behavior of the java.MqttClientPersistence interface"""
+class MqttClientPersistence(object):
+    """Mimic the behavior of the java.MqttClientPersistence interface.
+
+    Compatible with MQTT v3.
+
+    See: https://www.eclipse.org/paho/files/javadoc/org/eclipse/paho/client/mqttv3/MqttClientPersistence.html
+    """
+
+    __metaclass__ = ABCMeta
+
     def __init__(self):
         pass
 
+    def clear(self):
+        """Clears persistence, so that it no longer contains any persisted data.
+        """
+        pass
+
+    def close(self):
+        """Close the persistent store that was previously opened.
+        """
+        pass
+
+    def containsKey(self, key):
+        """Returns whether or not data is persisted using the specified key.
+
+        :param key The key for data, which was used when originally saving it.
+        :type key str
+        :return True if key is present.
+        """
+        pass
+
+    def get(self, key):
+        """Gets the specified data out of the persistent store.
+
+        :param key The key for the data to be removed from the store.
+        :type key str
+        :return The wanted data.
+        :type bytearray
+        """
+        pass
+
+    def keys(self):
+        """Returns an Enumeration over the keys in this persistent data store.
+
+        :return: generator
+        """
+        pass
+
+    def open(self, clientId, serverUri):
+        """Initialise the persistent store.
+
+        Initialise the persistent store. If a persistent store exists for this client ID then open it,
+        otherwise create a new one. If the persistent store is already open then just return. An application
+        may use the same client ID to connect to many different servers, so the client ID in conjunction
+        with the connection will uniquely identify the persistence store required.
+
+        :param clientId The client for which the persistent store should be opened.
+        :type clientId str
+        :param serverUri The connection string as specified when the MQTT client instance was created.
+        :type serverUri str
+        """
+        pass
+
     def put(self, key, persistable):
+        """Puts the specified data into the persistent store.
+
+        :param key The key for the data, which will be used later to retrieve it.
+        :type key str
+        :param persistable The data to persist.
+        :type persistable bool
+        """
+        pass
+
+    def remove(self, key):
+        """Remove the data for the specified key.
+
+        :param key The key associated to the data to remove.
+        :type key str
+        :return None
+        """
         pass
