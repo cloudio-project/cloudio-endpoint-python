f24a6d4df1d552bcf02f643944469c6fbbeddff8
boozo-unlimited <thomas.sterren@gmail.com>
2017-08-23 12:11:09 +0200

    Updated cloudio init file to allow sub-packages in 'cloudio' package located elsewhere
    Signed-off-by: boozo-unlimited <thomas.sterren@gmail.com>

--- src/cloudio/__init__.py
+++ src/cloudio/__init__.py
@@ -2,3 +2,8 @@
 
 __author__ = 'Thomas Sterren'
 __version__ = '0.1.0'
+
+# Tell python that there are more sub-packages present, physically located elsewhere.
+# See: https://stackoverflow.com/questions/8936884/python-import-path-packages-with-the-same-name-in-different-folders
+import pkgutil
+__path__ = pkgutil.extend_path(__path__, __name__)
