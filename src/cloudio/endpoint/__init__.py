41fee5f7b15b4fe0aa8e98770597cfcc156a7c17
Thomas Sterren <thomas.sterren@gmail.com>
2018-11-25 15:13:42 +0100

    Python 3.x compatibility

--- src/cloudio/endpoint/__init__.py
+++ src/cloudio/endpoint/__init__.py
@@ -3,5 +3,11 @@
 __author__ = 'Thomas Sterren'
 __version__ = '0.2.7'
 
-from endpoint import CloudioEndpoint
-from endpoint import version
+import six
+
+if six.PY3:
+    from .endpoint import CloudioEndpoint
+    from .endpoint import version
+else:
+    from endpoint import CloudioEndpoint
+    from endpoint import version
